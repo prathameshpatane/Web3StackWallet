addbefbeb0145d2113a78b82f867122f09390be2
Sneha Vaish <snehavaish44@gmail.com>
2026-05-28 02:38:00 +0530

    Fix static files and migrations for Vercel

--- cryptovault-backend/build_files.sh
+++ cryptovault-backend/build_files.sh
@@ -1,7 +1,5 @@
-#!/bin/bash
-
-# Install all dependencies
 pip install -r requirements.txt
 
-# Collect static files for Django admin
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
+
+python manage.py migrate
